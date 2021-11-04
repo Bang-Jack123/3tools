372a45ca4f92579c2883d655d27bbb7f9a69dcc0
Bang-Jack123 <93056023+Bang-Jack123@users.noreply.github.com>
2021-11-04 21:29:26 +0700

    README.md
    Install on termux
    
    $ pkg install Bash
    $ pkg install git
    $ git clone https://github.com/Bang-Jack123/3tools
    $ cd 3tools
    $ sh jajal.sh

---
+++ jajal.sh
@@ -0,0 +1,95 @@
+#!usr/bin/sh
+clear
+# code warna	
+blue='\033[34;1m'	
+green='\033[32;1m'		
+purple='\033[35;1m'			
+cyan='\033[36;1m'				
+red='\033[31;1m'					
+white='\033[37;1m'						
+yellow='\033[33;1m'							
+read -p "Siapa Nama Kaka: " name;
+clear	
+sleep 1		
+echo $cyan		
+figlet Hay $name		
+sleep 1					
+echo $blue">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo       $yellow "PEMBUAT ==> M R P A T R I X "
+echo ' Team ==> G4NO S3K T34M [!] '
+echo $blue">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo
+sleep 1
+	echo $green">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+	echo $green"1"$green")"$green" Subscribe Auothor"
+	echo $green"2"$cyan")"$green" Join Grup"
+	echo $blue"3"$blue")"$blue" Masuk scrip Spam-wa"
+	echo $red"4"$red")"$red" Masuk Scrip Nambah Kontrol contoh: CTRL"
+	echo $cyan"5"$cyan")"$cyan" Masuk scrip Cr4ck fb "
+	echo $cyan"6"$cyan")"$cyan" Masuk Tools Virus "
+	echo $blue"7"$blue")"$blue" Keluar"
+	echo $green">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+
+	sleep 1
+	read -p " Masukan pilihan anda : " pill
+	if [ $pill = 1 ]; then
+	sleep 1
+	    figlet loading
+	          xdg-open https://youtube.com/channel/UCSJohuQCtqfD2P73Z65g8jg
+	    sh jajal.sh
+fi
+
+	if [ $pill = 2 ];then
+	sleep 1
+	xdg-open https://chat.whatsapp.com/K7uNXDBZTqxGG10EeBi1SG
+	sh jajal.sh
+fi
+	if [ $pill = 3 ];then	
+	echo		
+	figlet sabar | lolcat		
+	git clone https://github.com/Bang-Jack123/Spam-wa
+	echo
+	clear
+	sleep 2		
+	cd Spam-wa		
+	sleep 3				
+	python2 WA.py				
+fi
+	if [ $pill = 4 ];then
+	echo
+	figlet sabar yah bang | lolcat
+	git clone https://github.com/Bang-Jack123/termux
+	echo
+	clear		
+	sleep 1			
+	cd termux			
+	sleep 1					
+	python2 termux.py
+fi
+	if [ $pill = 5 ];then		
+	echo					
+	figlet sabar | lolcat				
+	git clone https://github.com/Bang-Jack123/dihi		
+	echo
+	clear
+	sleep 1
+	cd dihi
+	sleep 2
+	python2 dihi.py
+fi
+	if [ $pill = 6 ]; then
+  	    echo
+	    echo "sabar loading"
+	    git clone https://github.com/mrvirusrecord/link-virus
+	    echo
+	    clear
+	    sleep 2
+	    cd link-virus
+	    sleep 3
+	    sh download.sh
+fi
+	if [ $pill = 7 ];then
+	echo "[!] TERIMA KASIH TELAH MENGGUNAKAN TOOLS SAYA MAAF KALO SCRIP NYA AGAK BERANTAKAN"
+	echo "[?]  WA ME : 081575706018 "
+	exit
+	fi
